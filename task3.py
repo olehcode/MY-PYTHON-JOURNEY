4b0909ca4326b171cb4b1622621ff9c9e90053d2
olehcode <142100924+olehcode@users.noreply.github.com>
2023-08-13 16:44:54 +0200

    task3.py
    datetime task

---
+++ task3.py
@@ -0,0 +1,4 @@
+#3. Write a Python program to display the current date and time.
+import datetime #imported module
+now=datetime.time.now() #asigning value and creating variable now,calling to module(datetime.) and then to class(time.) and method(now())
+print(now)#printing to console a variabe now
