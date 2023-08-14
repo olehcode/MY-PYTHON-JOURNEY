5918343dd86703d3c41452272a91fdd0c0daa78a
olehcode <142100924+olehcode@users.noreply.github.com>
2023-08-14 16:09:07 +0200

    datetime.py
    datetime module and datetime classes with atributes(functions) today(), now()

---
+++ datetime.py
@@ -0,0 +1,15 @@
+import datetime
+now = datetime.datetime.now()
+print("TODAY IS: ", now)
+
+"""I can use 2 methods of printing data #1:
+import datetime
+now = datetime.datetime.now()
+print("TODAY IS: ", now)
+#2 from datetime import datetime
+now= datetime.now()
+print("TODAY IS: ", now)
+
+!!!FOR ONLY DATE WE USE!!! datetime.date.today() ///
+FOR DATE AND TIME WE USE!!! datetime.datetime.now()
+"""
