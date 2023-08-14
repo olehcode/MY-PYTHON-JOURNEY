206c3968042cba05069003bde23b8c23455c759e
olehcode <142100924+olehcode@users.noreply.github.com>
2023-08-14 21:12:06 +0200

    Create timeinseconds.py

---
+++ timeinseconds.py
@@ -0,0 +1,3 @@
+from datetime import timedelta
+t = timedelta(days=12, hours=4, seconds=46, microseconds=326) #asigning value to a mew variable, and data to timedelta(days,hours,seconds, microseconds)ALWAYS SINTAXIS LIKE THIS !!!!(NAME=
+print("TIME IN SECONDS IS... ", t.total_seconds())#printing what we want, time in seconds
